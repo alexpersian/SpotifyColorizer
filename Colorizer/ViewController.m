92202e77daa7161b34333a360b9a4f35ae5c28d6
Alex Persian <alex_persian@me.com>
2020-02-07 14:15:53 -0500

    Implements basic UIImagePicker interface for select an image to colorize

--- Colorizer/ViewController.m
+++ Colorizer/ViewController.m
@@ -10,14 +10,53 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+@property (nonatomic) UIImagePickerController *imagePicker;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setup];
 }
 
+- (void)setup {
+    _imagePicker = [[UIImagePickerController alloc] init];
+    _imagePicker.delegate = self;
+    _imagePicker.allowsEditing = NO;
+    _imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    _photoImageView.contentMode = UIViewContentModeScaleAspectFit;
+    _photoImageView.image = [UIImage imageNamed:@"SpotifyLogoBlack"];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    if ([info valueForKey:UIImagePickerControllerOriginalImage] != NULL) {
+        UIImage *pickedImage = [info valueForKey:UIImagePickerControllerOriginalImage];
+        _photoImageView.image = pickedImage;
+    } else {
+        NSLog(@"Error: Unable to find original image.");
+    }
+
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (IBAction)tappedSelectPhoto:(id)sender {
+    NSLog(@"Opening image picker...");
+    [self presentViewController:_imagePicker animated:YES completion:NULL];
+}
+
+- (IBAction)tappedColorize:(id)sender {
+    NSLog(@"Colorizing...");
+
+    // Grey scale the image
+
+    // Apply color overlay to image
+
+    // Display image back into image view
+    
+}
 
 @end
